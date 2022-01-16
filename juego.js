6af3b6b6d9b84dc135e29c9d015d26fd4976aed0
Alan Yanse <yansealan@gmail.com>
2022-01-16 13:45:56 -0300

    update repository

--- juego.js
+++ juego.js
@@ -1,7 +1,19 @@
 
+
+function inicializa(){
+	
+	lienzo = document.getElementById("lienzo");
+	
+	ctx = lienzo.getContext("2d");
+	
+	cargaImagenes();
+	
+};
+
+
 //-------------Zona de Variables-----------------------
 
-let personaje = {x : 50 , y : 150, vy: 0, gravity : 2, salto : 20, vymax : 9, saltando : false, caminando : false}; // Array o struct de propiedades
+let personaje = {x : 50 , y : 150, vy: 0, gravity : 2, salto : 20, vymax : 9, saltando : false, caminando : false, caminando_pa_tras: false}; // objeto de json
 
 let fps = 25;
 
@@ -24,23 +36,13 @@ setInterval(function(){
 
 }, 1000/fps);
 
+
+
 function principal(){
 	
 	borrarLienzo();
 	gravedad();
 	dibujarPersonaje()
-};
-
-
-
-function inicializa(){
-	
-	lienzo = document.getElementById("lienzo");
-	
-	ctx = lienzo.getContext("2d");
-	
-	cargaImagenes();
-	
 };
 
 
@@ -54,6 +56,7 @@ function borrarLienzo(){
 	
 	
 };
+
 
 
 
@@ -98,9 +101,13 @@ function cargaImagenes(){
 	
 	img_personaje2.src = "imagenes/personaje2_der.png";
 	
-	//img_personaje3. = new Image();
+	img_personaje3 = new Image();
 	
-	//img_personaje3.src = "imagenes/personaje_izq1.png";
+	img_personaje3.src = "imagenes/personaje_izq1.png";
+	
+	img_personaje4 = new Image();
+	
+	img_personaje4.src = "imagenes/personaje2_izq.png";
 	
 }
 
@@ -116,13 +123,28 @@ function dibujarPersonaje(){
 			ctx.drawImage(img_personaje, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
 			
 			personaje.caminando = true;
-			//console.log(personaje.caminando);
+			
 	}
 	else{
 		
 			ctx.drawImage(img_personaje2, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
 			
-			//console.log(personaje.caminando);
+			
+		
+	}
+	
+	if (personaje.caminando_pa_tras == false){
+		
+			//ctx.drawImage(img_personaje, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
+			
+			personaje.caminando_pa_tras = true;
+			
+	}
+	else{
+		
+			//ctx.drawImage(img_personaje2, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
+			
+		
 		
 	}
 	
@@ -159,17 +181,17 @@ function caminar(){
 
 function caminarPaTras(){
 	
-	if (personaje.caminando == false){
+	if (personaje.caminando_pa_tras == false){
 		
-			personaje.caminando = true;
+			personaje.caminando_pa_tras = true;
 	
 			personaje.x -= 10;
 						
 	}
 	
-	if (personaje.caminando == true){
+	if (personaje.caminando_pa_tras == true){
 		
-			personaje.caminando = false;
+			personaje.caminando_pa_tras = false;
 	
 			personaje.x -= 10;
 						
