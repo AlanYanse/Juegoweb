671772441e9247aa290da00ffd82e5ef87a2444d
Alan Yanse <yansealan@gmail.com>
2022-02-13 11:16:35 -0300

    first commit in the develop branch

--- juego.js
+++ juego.js
@@ -1,19 +1,8 @@
 
 
-function inicializa(){
-	
-	lienzo = document.getElementById("lienzo");
-	
-	ctx = lienzo.getContext("2d");
-	
-	cargaImagenes();
-	
-};
-
-
 //-------------Zona de Variables-----------------------
 
-let personaje = {x : 50 , y : 150, vy: 0, gravity : 2, salto : 20, vymax : 9, saltando : false, caminando : false, caminando_pa_tras: false}; // objeto de json
+let personaje = {x : 50 , y : 150, vy: 0, gravity : 2, salto : 20, vymax : 9, saltando : false, caminando : false}; // Array o struct de propiedades
 
 let fps = 25;
 
@@ -31,32 +20,41 @@ let lienzo, ctx;
 
 
 setInterval(function(){
-	
+
 	principal();
 
 }, 1000/fps);
 
-
-
 function principal(){
-	
+
 	borrarLienzo();
 	gravedad();
 	dibujarPersonaje()
 };
 
 
-function borrarLienzo(){
-	
-	// propiedades del canvas
-	
-	lienzo.width = ancho; 
-	
-	lienzo.height = alto;
-	
-	
+
+function inicializa(){
+
+	lienzo = document.getElementById("lienzo");
+
+	ctx = lienzo.getContext("2d");
+
+	cargaImagenes();
+
 };
 
+
+function borrarLienzo(){
+
+	// propiedades del canvas
+
+	lienzo.width = ancho; 
+
+	lienzo.height = alto;
+
+
+};
 
 
 
@@ -64,27 +62,27 @@ function borrarLienzo(){
 
 
 document.addEventListener("keydown", function(evento){
-	
+
 	if (evento.keyCode == 32){
 		console.log("Salta");
-		
+
 		saltar();
-		
+
 	}
 	if (evento.keyCode == 39){
 		console.log("Camina pa' delante");
-		
+
 		caminar();
-		
+
 	}
 	if (evento.keyCode == 37){
 		console.log("Camina pa' tras");
-		
+
 		caminarPaTras();
-		
+
 	}
-	
-	
+
+
 });
 
 //-----------------Empezando a trabajar con las imágenes-------------------
@@ -92,23 +90,19 @@ document.addEventListener("keydown", function(evento){
 
 
 function cargaImagenes(){
-	
+
 	img_personaje = new Image();
-	
+
 	img_personaje.src = "imagenes/personaje1_der.png";
-	
+
 	img_personaje2 = new Image();
-	
+
 	img_personaje2.src = "imagenes/personaje2_der.png";
-	
-	img_personaje3 = new Image();
-	
-	img_personaje3.src = "imagenes/personaje_izq1.png";
-	
-	img_personaje4 = new Image();
-	
-	img_personaje4.src = "imagenes/personaje2_izq.png";
-	
+
+	//img_personaje3. = new Image();
+
+	//img_personaje3.src = "imagenes/personaje_izq1.png";
+
 }
 
 
@@ -116,40 +110,25 @@ function cargaImagenes(){
 
 
 function dibujarPersonaje(){
-	
-	
+
+
 	if (personaje.caminando == false){
-		
+
 			ctx.drawImage(img_personaje, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
-			
+
 			personaje.caminando = true;
-			
+			//console.log(personaje.caminando);
 	}
 	else{
-		
+
 			ctx.drawImage(img_personaje2, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
-			
-			
-		
+
+			//console.log(personaje.caminando);
+
 	}
-	
-	if (personaje.caminando_pa_tras == false){
-		
-			//ctx.drawImage(img_personaje, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
-			
-			personaje.caminando_pa_tras = true;
-			
-	}
-	else{
-		
-			//ctx.drawImage(img_personaje2, 0, 0, 46, 46, personaje.x, personaje.y, 50, 50);
-			
-		
-		
-	}
-	
-	
-	
+
+
+
 }
 
 
@@ -158,86 +137,82 @@ function dibujarPersonaje(){
 
 
 function caminar(){
-	
+
 	if (personaje.caminando == false){
-		
+
 		personaje.caminando = true;
-	
+
 		personaje.x += 10;
-						
+
 	}
-	
+
 	if (personaje.caminando == true){
-		
+
 		personaje.caminando = false;
-	
+
 		personaje.x += 10;
-						
+
 	}
-	
-	
-	
+
+
+
 }
 
 function caminarPaTras(){
-	
-	if (personaje.caminando_pa_tras == false){
-		
-			personaje.caminando_pa_tras = true;
-	
+
+	if (personaje.caminando == false){
+
+			personaje.caminando = true;
+
 			personaje.x -= 10;
-						
+
 	}
-	
-	if (personaje.caminando_pa_tras == true){
-		
-			personaje.caminando_pa_tras = false;
-	
+
+	if (personaje.caminando == true){
+
+			personaje.caminando = false;
+
 			personaje.x -= 10;
-						
+
 	}
-	
-	
-	
+
+
+
 }
 
 function saltar(){
-	
+
 		personaje.saltando = true;
-	
+
 		personaje.vy = personaje.salto;
-	
-	
+
+
 }
 
 function gravedad(){
-	
+
 	if (personaje.saltando == true){
-		
+
 		if (personaje.y > 150){
-			
+
 				personaje.saltando = false;
-			
+
 				personaje.vy = 0;
-			
+
 				personaje.y = 150;
 		}
 		else{
-			
+
 			personaje.vy -= personaje.gravity;
-		
+
 			personaje.y -= personaje.vy;
-			
-			
+
+
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 }
 
-
-
-
-	
